4d06747fa5a2f158ebbd1dceea272e32d8e637da
sele21 <259571723+sele21@users.noreply.github.com>
2026-02-12 13:41:10 +0300

    Created Category, Recipe, Ingredient, Step, RecipeImage models

--- apps/recipes/models.py
+++ apps/recipes/models.py
@@ -22,6 +22,7 @@ class Recipe(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     
     prep_time = models.IntegerField(help_text="Preparation time in minutes")
+    cook_time = models.IntegerField(help_text="Cooking time in minutes")
     servings = models.IntegerField(default=1)
     
     featured_image = models.ImageField(upload_to='recipes/featured/', blank=True, null=True)
