4fe201446cc7ae00b7b64a52b0af1855a2974504
sele01 <sele.edu21@gmail.com>
2026-07-16 14:48:45 +0300

    Safe-ify database queries on home page

--- apps/core/views.py
+++ apps/core/views.py
@@ -11,11 +11,29 @@ class HomeView(TemplateView):
         context = super().get_context_data(**kwargs)
 
         # Statistics
-        context['recipe_count'] = Recipe.objects.filter(is_published=True).count()
-        context['user_count'] = User.objects.count()
-        context['category_count'] = Category.objects.count()
-        context['total_likes'] = 0  # You can calculate this if you have a Like model
+        try:
+            context['recipe_count'] = Recipe.objects.filter(is_published=True).count()
+        except Exception:
+            context['recipe_count'] = 0
 
+        try:
+            context['category_count'] = Category.objects.count()
+        except Exception:
+            context['category_count'] = 0
+
+        try:
+            context['featured_recipes'] = Recipe.objects.filter(is_published=True).order_by('-created_at')[:6]
+        except Exception:
+            context['featured_recipes'] = []
+
+        try:
+            context['categories'] = Category.objects.all()
+        except Exception:
+            context['categories'] = []
+        try:
+            context['user_count'] = User.objects.count()
+        except Exception:
+            context['user_count'] = 0
         # Featured recipes (latest 6)
         context['featured_recipes'] = Recipe.objects.filter(is_published=True).order_by('-created_at')[:6]
 
